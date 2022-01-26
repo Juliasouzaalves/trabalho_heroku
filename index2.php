ad0d7b368703ef55938b36d164391fee8a650bf8
Juliasouzaalves <juliasouzaalves8@gmail.com>
2022-01-26 11:06:13 -0300

    first commit

---
+++ index2.php
@@ -0,0 +1,43 @@
+<?php 
+
+?> 
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="csstelalogin.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/21b4e8094d.js" crossorigin="anonymous"> </script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+    <title>Tela Login</title>
+    <style>
+        h2 {
+            font-family: 'Dancing Script', cursive;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="geral">
+        <h1> FLEMG </h1>
+        <h2> Faça login para comprar seu ingresso </h2>
+        <div class="icon">
+            <i class="fas fa-book-reader icon-modify"></i> 
+            <br> </br> 
+            
+            <div class="input">
+            <input type="email" placeholder="Insira seu E-mail"  size="50" width="70" /> 
+            </div> 
+            
+            <input type="password" placeholder="Insira sua senha" size="50" width="70" /> 
+            <button> Confirmar</button>
+        </div>
+    </div> 
+</body>
+
+</html>
